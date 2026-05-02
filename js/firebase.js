a223b703012b35957cfa146b7632817238467a79
Rehan Raza <099rehan@gmail.com>
2026-05-02 23:43:04 +0530

    Update Firebase configuration for new project

--- js/firebase.js
+++ js/firebase.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAtIwHtqhlezHWFUvoDrd63Zot9_vk3ZBQ",
-    authDomain: "online-examination-syste-c82c7.firebaseapp.com",
-    databaseURL: "https://online-examination-syste-c82c7-default-rtdb.asia-southeast1.firebasedatabase.app/",
-    projectId: "online-examination-syste-c82c7",
-    storageBucket: "online-examination-syste-c82c7.firebasestorage.app",
-    messagingSenderId: "259452851401",
-    appId: "1:259452851401:web:3a769d19499e04adbf6918"
+    apiKey: "AIzaSyCTQTEpZTUjDph723-pYEbDBk61hfIgsmU",
+    authDomain: "testhub-d56d5.firebaseapp.com",
+    databaseURL: "https://testhub-d56d5-default-rtdb.firebaseio.com/",
+    projectId: "testhub-d56d5",
+    storageBucket: "testhub-d56d5.firebasestorage.app",
+    messagingSenderId: "977890685081",
+    appId: "1:977890685081:web:4a032c82847e5cfaa6c808"
 };
 
 firebase.initializeApp(firebaseConfig);
